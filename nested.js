91767b2041ea2ea0461555d880ccd825e55c8635
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-01-24 17:27:57 +0600

    nested conditions

---
+++ nested.js
@@ -0,0 +1,16 @@
+const money = -20;
+
+if(money >= 300){
+    console.log('bro you are rich!!!');
+}else{
+    if(money > 100){
+        console.log('tui gorib o na boro lok o na');
+    }else{
+        if(money > 0){
+            console.log('dosto kola kha energy bara');
+        }
+        else{
+            console.log('tui kono tk chaibi na');
+        }
+    }
+}
