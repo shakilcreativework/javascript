aed979bd0e22cb4d6aed42aec4d39c3edc7772e1
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-01-24 15:44:57 +0600

    learning if, else conditions

---
+++ if.js
@@ -0,0 +1,21 @@
+// if(3 < 10){
+//     console.log('I have a smaller value');
+// }
+// if(13 < 10){
+//     console.log('I have a smaller value');
+// }
+
+
+// var biryaniPrice = 250;
+
+// if(biryaniPrice < 300){
+//     console.log('mama give me som biryani');
+// }
+
+var weight = 20;
+
+if(weight < 20){
+    console.log('I will carry it by myself');
+}else{
+    console.log('I will rent a rickshaw');
+}
