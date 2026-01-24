a44fc920f8751d98a6d7013b187eb32fa79978a2
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-01-24 18:13:52 +0600

    shortcut conditions

---
+++ shortcut.js
@@ -0,0 +1,31 @@
+const isLeader = true;
+
+// if(isLeader === true){
+//     console.log('leader asche! chair charo');
+// }else{
+//     console.log('vitore dukte dibi na');
+// }
+
+
+// if(isLeader){
+//     console.log('leader asche! chair charo');
+// }else{
+//     console.log('vitore dukte dibi na');
+// }
+
+
+const isPassed = true;
+
+// if(isPassed === false){
+//     console.log('toke biya diya dimu');
+// }else{
+//     console.log('tumi amader noyon er moni');
+// }
+
+
+if(!isPassed){
+    console.log('toke biya diya dimu');
+}else{
+    console.log('tumi amader noyon er moni');
+}
+
