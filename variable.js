f4665869a25b5e45f18642a6c352087ef7d19d18
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-01-24 10:05:53 +0600

    learning math and variable operations

---
+++ variable.js
@@ -0,0 +1,14 @@
+/**
+ * VARIABLE ==> vary + able
+ * 5 things to declare a variable
+*/ 
+
+var weight = 38;
+var price = 39;
+var year = 1996;
+var money = 2005;
+
+var age;
+console.log(age);
+console.log(money);
+console.log(price);
