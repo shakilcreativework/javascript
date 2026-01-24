9ea89f120331714cede984c58a1fcd8061319482
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-01-24 17:04:34 +0600

    multiple conditions

---
+++ multiple.js
@@ -0,0 +1,32 @@
+const salary = 25001;
+const isBCS = true;
+const height = 78;
+const hasCar = false;
+
+// if(salary > 20000 && height > 66){
+//     console.log('Su....patro');
+// }else{
+//     console.log('onno patro khuji');
+// }
+
+
+// if(salary > 25000 || height > 72){
+//     console.log('eso sona kobul');
+// }else{
+//     console.log('vag tui mokbul');
+// }
+
+
+// more and more condition
+// if(salary > 25000 || height > 72 || isBCS == true){
+//     console.log('eso sona kobul');
+// }else{
+//     console.log('vag tui mokbul');
+// }
+
+
+if((salary > 25000 && hasCar == true) || isBCS == true){
+    console.log('choddo gusti raji eso sona kobul');
+}else{
+    console.log('vag tui mokbul');
+}
