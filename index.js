cd564068930cd12cfafbaffc4ccef57aa8dec24b
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-01-27 21:33:05 +0600

    index of array learning was amazing

---
+++ index.js
@@ -0,0 +1,10 @@
+const numbers = [66, 36, 63, 74, 84];
+
+console.log(numbers);
+console.log(numbers[4]);
+
+const fourth = [numbers[3]];
+console.log(fourth);
+
+numbers[1] = 999;
+console.log(numbers);
