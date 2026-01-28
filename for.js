2d15d66aa1a1305fde845bb8311bdb935fb97d77
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-01-28 12:02:08 +0600

    learning for and while loop defference ways

---
+++ for.js
@@ -0,0 +1,9 @@
+let num = 1;
+while(num < 5){
+    // console.log(num);
+    num++;
+}
+
+for(let num = 0; num < 5;  num++){
+    console.log(num);
+};
---
+++ odd-even.js
@@ -0,0 +1,15 @@
+for(let i = 1; i <= 50; i++){
+    // console.log(i);
+}
+
+
+// even
+for(let i = 0; i <=20; i=i+2){
+    // console.log(i);
+}
+
+
+// odd
+for(let i = 1; i <=20; i=i+2){
+    console.log(i);
+}
