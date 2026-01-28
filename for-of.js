d8bfb7512c7c43f073fb16fda69a16bf7600a6f5
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-01-28 10:40:31 +0600

    amazing js learning start today

---
+++ for-of.js
@@ -0,0 +1,4 @@
+const numbers = [12, 58, 58, 530, 84, 38];
+
+console.log(numbers);
+
---
+++ recap.js
@@ -0,0 +1,15 @@
+// const: value will not change
+// let: value can change
+// var: we should not use it
+
+// variable
+const amount = 200;
+
+// condition
+if(amount > 100){
+
+}else{
+
+}
+
+// array
