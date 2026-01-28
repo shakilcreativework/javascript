fdadd6af0466ac474d92c8c25f1b214888dd63ca
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-01-28 11:07:49 +0600

    while loop learning

--- for-of.js
+++ for-of.js
@@ -1,4 +1,14 @@
 const numbers = [12, 58, 58, 530, 84, 38];
 
-console.log(numbers);
+// console.log(numbers);
 
+for(const num of numbers){
+    // console.log(num);
+}
+
+
+const fruits = ['orange', 'apple', 'banana', 'jackfruit', 'watermelon'];
+
+for(const fruit of fruits){
+    // console.log(fruit);
+}
---
+++ while.js
@@ -0,0 +1,17 @@
+const number = 7;
+
+if (number < 10){
+    // console.log('choto ekta number paichi');
+}
+// console.log('something new is cooking');
+
+
+// --------while loop-----------
+let num = 0;
+
+while(num < 5){
+    console.log('loooping' + ' ' + num);
+    // num = num + 1;
+    // num += 1;
+    num++;
+}
